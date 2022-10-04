6e4f90aa177fc16ce225fad4a98515ea5d14b9aa
matt.braunwart <matt.braunwart@gmail.com>
2022-10-04 13:18:02 -0400

    End of session updates

--- Session 1 - Terraform Cloud/tfc_module/terraform.tf
+++ Session 1 - Terraform Cloud/tfc_module/terraform.tf
@@ -8,7 +8,7 @@ terraform {
   cloud {
     # organization = "insight"
     # workspaces {
-    #   name = "mrb-terraform-topics"
+    #   name = "mrb-terraform-topics-module"
     # }
   }
 }
@@ -17,6 +17,8 @@ provider "github" {
 }
 
 module "repo" {
+  source  = "app.terraform.io/insight/repository-management/github"
+  version = "0.0.1"
 
   application_name       = "test-repo"
   repository_prefix      = "terraform"
@@ -34,4 +36,13 @@ module "repo" {
     visibility             = "internal"
     vulnerability_alerts   = true
   }
+
+  repository_environments = [ {
+    custom_branches = false
+    name = "dev"
+    protected_branches = false
+    team_reviewers = [ ]
+    user_reviewers = [ ]
+    wait_timer = 1
+  } ]
 }
